42a0c8b67b6224fa4adf670d50d545d2c843531d
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-26 08:51:31 +0330

    create first thread

---
+++ thread.py
@@ -0,0 +1,13 @@
+import threading
+import time
+
+def sleeper(n, name):
+    print(f'Hi, I am {name}')
+    time.sleep(n)
+    print(f'{name} has woken up from sleep')
+
+# Create a Thread object and start the thread
+t = threading.Thread(target=sleeper, args=(5, 'sepdie'))
+
+t.start()
+print('hello')
